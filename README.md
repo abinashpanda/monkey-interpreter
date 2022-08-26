b842b3c97847b6481999291ca88f0697e153b3f4
Abinash Panda <abinash@prodios.com>
2022-08-26 15:17:59 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# monkey-interpreter
+
+Interpreter for Monkey Programming Langugage. The codebase is derived from [Writing an interpreter in Go](https://interpreterbook.com/) book.
+
+## REPL
+
+Run
+
+```sh
+go build -o monkey-interpreter && ./monkey-interpreter
+```
---
+++ lexer/lexer.go
@@ -0,0 +1,135 @@
+package lexer
+
+import (
+	"monkey-interpreter/token"
+)
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	lexer := &Lexer{input: input}
+	lexer.readChar()
+	return lexer
+}
+
+func (lexer *Lexer) skipWhitespace() {
+	for lexer.ch == ' ' || lexer.ch == '\t' || lexer.ch == '\n' || lexer.ch == '\r' {
+		lexer.readChar()
+	}
+}
+
+func (lexer *Lexer) readChar() {
+	if lexer.readPosition >= len(lexer.input) {
+		lexer.ch = 0
+	} else {
+		lexer.ch = lexer.input[lexer.readPosition]
+	}
+	lexer.position = lexer.readPosition
+	lexer.readPosition += 1
+}
+
+func (lexer *Lexer) peekChar() byte {
+	if lexer.readPosition >= len(lexer.input) {
+		return 0
+	}
+	return lexer.input[lexer.readPosition]
+}
+
+func (lexer *Lexer) readIdentifier() string {
+	position := lexer.position
+	for isLetter(lexer.ch) {
+		lexer.readChar()
+	}
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) readNumber() string {
+	position := lexer.position
+	for isDigit(lexer.ch) {
+		lexer.readChar()
+	}
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	lexer.skipWhitespace()
+
+	switch lexer.ch {
+	case '=':
+		if lexer.peekChar() == '=' {
+			ch := lexer.ch
+			lexer.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(lexer.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, lexer.ch)
+		}
+	case '+':
+		tok = newToken(token.PLUS, lexer.ch)
+	case '-':
+		tok = newToken(token.MINUS, lexer.ch)
+	case '!':
+		if lexer.peekChar() == '=' {
+			ch := lexer.ch
+			lexer.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(lexer.ch)}
+		} else {
+			tok = newToken(token.BANG, lexer.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, lexer.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, lexer.ch)
+	case '<':
+		tok = newToken(token.LT, lexer.ch)
+	case '>':
+		tok = newToken(token.GT, lexer.ch)
+	case ',':
+		tok = newToken(token.COMMA, lexer.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, lexer.ch)
+	case '(':
+		tok = newToken(token.LPAREN, lexer.ch)
+	case ')':
+		tok = newToken(token.RPAREN, lexer.ch)
+	case '{':
+		tok = newToken(token.LBRACE, lexer.ch)
+	case '}':
+		tok = newToken(token.RBRACE, lexer.ch)
+	case 0:
+		tok = token.Token{Type: token.EOF, Literal: ""}
+	default:
+		if isLetter(lexer.ch) {
+			tok.Literal = lexer.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(lexer.ch) {
+			tok.Literal = lexer.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lexer.ch)
+		}
+	}
+
+	lexer.readChar()
+	return tok
+}
